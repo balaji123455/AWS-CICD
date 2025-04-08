726821da909a7c60eebd10ecacbadb4afc75f0c4
balaji123455 <balajigan2002@gmail.com>
2025-04-08 08:08:10 +0100

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -6,4 +6,4 @@ set -e
 docker pull balaji495/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 6000:9000 balaji495/simple-python-flask-app:latest
+docker run -d -p 5000:5000 balaji495/simple-python-flask-app:latest
