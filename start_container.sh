397c9ef54bb55ccd11dfcd4cb14ae83657b6edb8
balaji123455 <balajigan2002@gmail.com>
2025-04-08 08:18:54 +0100

    Delete start_container.sh

--- start_container.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull balaji495/simple-python-flask-app:latest
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 balaji495/simple-python-flask-app:latest
